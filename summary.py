6478a52f973d916067d0ae0172352c351beb15e0
Hesham <61054665+h9-tect@users.noreply.github.com>
2023-01-02 15:42:26 +0200

    Update summary.py

--- summary.py
+++ summary.py
@@ -1,5 +1,3 @@
-Python 3.11.0 (main, Oct 24 2022, 18:26:48) [MSC v.1933 64 bit (AMD64)] on win32
-Type "help", "copyright", "credits" or "license()" for more information.
 import arxiv
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 ... 
